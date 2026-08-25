1e7f3a5b16f251afa9002d41c3c596958adf4ec8
Admin <admin@example.com>
2026-08-25 20:49:30 +0200

    merge CSS into single style.css from scratch - more human look

--- patch-demo.js
+++ patch-demo.js
@@ -66,8 +66,7 @@ function demoLogin() {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="color-scheme" content="dark">
     <title>Login - BEACON</title>${demoShim}
-    <link rel="stylesheet" href="css/base.css">
-    <link rel="stylesheet" href="css/components.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
